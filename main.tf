67a18d23e9cfd2ed87681190d85dd2c3da52236c
Eliora <elioranazariah@gmail.com>
2024-12-02 02:16:28 +0100

    help

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "azurerm_resource_group" "lightrsg" {
-  name     = "lightrsg"
-  location = "US EAST"
+resource "azurerm_resource_group" "US_EAST" {
+  name     = "my-resource-group"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "lightvnet" {
-  name                = "lightvnet"
-  address_space       = ["172.16.0.0/16"]
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.US_EAST.location
-  resource_group_name = azurerm_resource_group.lightrsg
+  resource_group_name = azurerm_resource_group.US_EAST.name
 }
