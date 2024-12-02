212d04998ba1c146f1d3bb4ad685fb2a727fa156
Eliora <elioranazariah@gmail.com>
2024-12-02 02:09:56 +0100

    with terraform init

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "lightrsg" {
   name     = "lightrsg"
-  location = "uscentral1"
+  location = "US EAST"
 }
 
 resource "azurerm_virtual_network" "lightvnet" {
   name                = "lightvnet"
   address_space       = ["172.16.0.0/16"]
-  location            = azurerm_resource_group.uscentral1.location
+  location            = azurerm_resource_group.US_EAST.location
   resource_group_name = azurerm_resource_group.lightrsg
 }
